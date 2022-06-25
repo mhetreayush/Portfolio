4c64d4b52746fb055ba60f975171dd4736353d35
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-06-25 18:18:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,43 @@ window.addEventListener('keydown', (e) => {
         e.preventDefault();
     }
 });
+function scrollToAnimationDepricated(x, y, animation_time = 100) {
+    // Animation time in milliseconds
+    let speed_x = -(window.scrollX - x) / animation_time // v = s/t #JEE
+    let speed_y = -(window.scrollY - y) / animation_time // v = s/t #JEE
+    let animation_id = setInterval(() => {
+        // This is happening every millisecond so you can calculate how much you will have to scroll per intravel
+        window.scrollBy(speed_x, speed_y) // Or you can use the other method where we use deltaTime which is more reliable
+    }, 1)
+    setTimeout(() => {
+        window.scrollTo(x, y)
+        clearInterval(animation_id)
+    }, animation_time)
+    // This will stop the animation after the time is over and set the scroll to the actual value for resolving errors in slower systems
+}
+
+function scrollToAnimation(x, y, animation_time = 100) {
+    // Animation time in seconds
+    let startTime = Date.now()
+
+    let deltaX = x - window.scrollX
+    let deltaY = y - window.scrollY
+
+    let startX = window.scrollX
+    let startY = window.scrollY
+    let animation_id = setInterval(() => {
+        frameTime = Date.now() - startTime // This is the time elapsed after the animation is started
+        window.scrollTo(
+            startX + deltaX * (frameTime / animation_time),
+            startY + deltaY * (frameTime / animation_time)
+        )
+    }, 0)
+    setTimeout(() => {
+        window.scrollTo(x, y)
+        clearInterval(animation_id)
+    }, animation_time)
+    // This will stop the animation after the time is over and set the scroll to the actual value for resolving errors in slower systems
+}
 
 document.body.onkeyup = function (e) {
     if (e.keyCode == 32) {
@@ -63,10 +100,10 @@ document.body.onkeyup = function (e) {
     }
 
     if (e.keyCode == 83) {
-        window.scrollBy(0, window.innerHeight-50);
+        scrollToAnimation(0, window.innerHeight-50);
     }
     if (e.keyCode == 87) {
-        window.scrollBy(0, -window.innerHeight+50);
+        scrollToAnimation(0, -window.innerHeight+50);
     }
 }
 
